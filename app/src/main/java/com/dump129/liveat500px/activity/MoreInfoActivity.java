cb40e2c1b4669a45c4540ef22e32a2c54ab0a911
dumposk129 <dumposk129@gmail.com>
2017-02-19 11:20:50 +0700

    Up Button

--- app/src/main/java/com/dump129/liveat500px/activity/MoreInfoActivity.java
+++ app/src/main/java/com/dump129/liveat500px/activity/MoreInfoActivity.java
@@ -2,6 +2,7 @@ package com.dump129.liveat500px.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.dump129.liveat500px.R;
 import com.dump129.liveat500px.fragment.MoreInfoFragment;
@@ -13,9 +14,25 @@ public class MoreInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
 
+        initInstance();
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentMoreInfo, MoreInfoFragment.newInstance()).addToBackStack(null).commit();
         }
+    }
+
+    private void initInstance() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
